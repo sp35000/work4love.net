c5b2d5526b7e594e6b7224d229b7298e800ec526
sp35000 <sp35000@yahoo.com.br>
2022-10-18 07:33:35 -0300

    feat: calculator and addThis included

--- archive/ce/compile.bat
+++ archive/ce/compile.bat
@@ -10,6 +10,7 @@ copy header.txt+fort08.txt+menu1.txt+footer.txt 08.html>nul
 copy header.txt+fort09.txt+menu1.txt+footer.txt 09.html>nul
 copy header.txt+fort10.txt+menu1.txt+footer.txt 10.html>nul
 copy header.txt+fort11.txt+menu1.txt+footer.txt 11.html>nul
+
 copy header.txt+fort12.txt+menu2.txt+footer.txt 12.html>nul
 copy header.txt+fort13.txt+menu2.txt+footer.txt 13.html>nul
 copy header.txt+fort14.txt+menu2.txt+footer.txt 14.html>nul
@@ -21,11 +22,15 @@ copy header.txt+fort19.txt+menu2.txt+footer.txt 19.html>nul
 copy header.txt+fort20.txt+menu2.txt+footer.txt 20.html>nul
 copy header.txt+fort21.txt+menu2.txt+footer.txt 21.html>nul
 copy header.txt+fort22.txt+menu2.txt+footer.txt 22.html>nul
+
 copy header.txt+fort23.txt+menu4.txt+footer.txt 23.html>nul
 copy header.txt+fort24.txt+menu4.txt+footer.txt 24.html>nul
+
 copy header.txt+fort25.txt+menu3.txt+footer.txt 25.html>nul
+
 copy header.txt+fort26.txt+menu2.txt+footer.txt 26.html>nul
 copy header.txt+fort27.txt+menu2.txt+footer.txt 27.html>nul
+
 copy header.txt+fort28.txt+menu3.txt+footer.txt 28.html>nul
 copy header.txt+fort29.txt+menu3.txt+footer.txt 29.html>nul
 copy header.txt+fort30.txt+menu3.txt+footer.txt 30.html>nul
@@ -35,9 +40,11 @@ copy header.txt+fort33.txt+menu3.txt+footer.txt 33.html>nul
 copy header.txt+fort34.txt+menu3.txt+footer.txt 34.html>nul
 copy header.txt+fort35.txt+menu3.txt+footer.txt 35.html>nul
 copy header.txt+fort36.txt+menu3.txt+footer.txt 36.html>nul
-copy header.txt+fort37.txt+menu3.txt+footer.txt 37.html>nul
-copy header.txt+fort38.txt+menu3.txt+footer.txt 38.html>nul
-copy header.txt+fort39.txt+menu3.txt+footer.txt 39.html>nul
+
+copy header.txt+fort37.txt+menu4.txt+footer.txt 37.html>nul
+copy header.txt+fort38.txt+menu4.txt+footer.txt 38.html>nul
+copy header.txt+fort39.txt+menu4.txt+footer.txt 39.html>nul
+
 copy header.txt+fort40.txt+menu2.txt+footer.txt 40.html>nul
 copy header.txt+fort41.txt+menu2.txt+footer.txt 41.html>nul
 copy header.txt+fort34.txt+menu3.txt+footer.txt index.html>nul
