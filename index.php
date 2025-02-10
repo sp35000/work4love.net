e2f199e684e04c68c207d0a490831b2910b36c7f
sp35000 <sp35000@yahoo.com.br>
2025-02-10 14:41:15 -0300

    fix: bootstrap fix

--- index.php
+++ index.php
@@ -16,11 +16,13 @@ include("./sig/include/menusup.php");
     <!-- <h1>Work4Love.net</h1> -->
     <h1><img src="/sig/images/Work4Love.net-logo.png" alt="Work4Love.net" width="40%"/></h1>
     <p class="bg-info"><big>Work4Love.net - Portal de serviços: Banco de dados de Notícias, Sites sobre Finanças, Tecnologia, Viagens, Cursos e Entretenimento, Galeria de fotos de viagem e Blog Nomad Fire Life.</big></p>
-    <div id="clock">day, dd/mm/yyyy hh:mm:ss</div>
-    <script>
-    setInterval(showTime, 120);
-    showTime();
-    </script>
+      <div id="clock">day, dd/mm/yyyy hh:mm:ss</div>
+      <script>
+      setInterval(showTime, 120);
+      showTime();
+      </script>
+      </div>
+    </div>
 
     <div class="col-md-12 columns text-center" style="padding-left:10%;padding-right:10%;padding-top:5%">
     <h2>Serviços</h2>
@@ -49,8 +51,8 @@ include("./sig/include/menusup.php");
 
       <div class="col-md-4 columns text-center">
       <p>
-        <a href="/nomad/" class="btn btn-lg btn-success" target="_blank">Blog</a><br/>
-        Nomad Fire Life - Independência Financeira como Nômade Digital.
+      <a href="/nomad/" class="btn btn-lg btn-success" target="_blank">Blog</a><br/>
+      Nomad Fire Life - Independência Financeira como Nômade Digital.
       </p>
       </div>
     </div>
@@ -58,8 +60,8 @@ include("./sig/include/menusup.php");
     <div class="col-md-12 columns text-center" style="padding-left:10%;padding-right:10%">
       <div class="col-md-4 columns text-center">
       <p>
-        <a href="https://www.mensagembr.com" class="btn btn-lg btn-success" target="_blank">MensagemBR</a><br/>
-        Site para envio de mensagens positivas.
+      <a href="https://www.mensagembr.com" class="btn btn-lg btn-success" target="_blank">MensagemBR</a><br/>
+      Site para envio de mensagens positivas.
       </p>
       </div>
 
@@ -78,14 +80,16 @@ include("./sig/include/menusup.php");
 
     <div class="col-md-12 columns text-center" style="padding-left:10%;padding-right:10%;padding-top:5%">
     <h2>Cotações</h2>
-    <div class="frame-container">
-    <iframe class="responsive-iframe" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vREdJFqdh2z9iss2b73Z3vvNywBhfGecHReDFeyUBL-S_DQMpV_Es76CKIPxa552_mhmmVb9p57B5Xe/pubhtml?gid=1413162259&amp;single=true&amp;widget=true&amp;headers=false" width="800px" height="400px">
-      <span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start">﻿</span>
-      <span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height:0;"class="mce_SELRES_start">﻿</span>
-    </iframe>
+      <div class="frame-container">
+      <iframe class="responsive-iframe" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vREdJFqdh2z9iss2b73Z3vvNywBhfGecHReDFeyUBL-S_DQMpV_Es76CKIPxa552_mhmmVb9p57B5Xe/pubhtml?gid=1413162259&amp;single=true&amp;widget=true&amp;headers=false" width="800px" height="400px">
+        <span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start">﻿</span>
+        <span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height:0;"class="mce_SELRES_start">﻿</span>
+        </iframe>
+      </div>
+      <p class="bg-info" align="center"><small>This spreadsheet is a courtesy of <strong><a href="https://rommiebr.com/" target="_blank">Rommie blog</a></strong>.</small></br></p>
     </div>
-    <p class="bg-info" align="center"><small>This spreadsheet is a courtesy of <strong><a href="https://rommiebr.com/" target="_blank">Rommie blog</a></strong>.</small></br></p>
 
+    <div class="col-md-12 columns text-center" style="padding-left:10%;padding-right:10%;padding-top:5%">
     <h2>Sobre este site</h2>
     <p>Inicialmente, este site foi criado para armazenar meus sites favoritos em um lugar centralizado na Internet.<br/>
     Ele já esteve hospedado inicialmente na Geocities, até chegar ao formato atual com domínio e hospedagem próprios.<br/>
@@ -94,7 +98,7 @@ include("./sig/include/menusup.php");
     O endereço Work4Love.net ficou sendo o portal para todos estes Serviços.</p>
     </div>
   </div>
-  <?php include("./sig/include/footer.php"); ?>
+<?php include("./sig/include/footer.php"); ?>
 </div>
 </body>
 </html>
