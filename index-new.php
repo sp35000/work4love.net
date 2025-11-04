432133ae95bdda47d0d89bc629ed5132cc8abcbc
yzmu <yzmu@pc18ck.yzmu>
2025-11-04 18:44:59 -0300

    portal bootstrap correction, news4 improved

--- index-new.php
+++ index-new.php
@@ -14,8 +14,9 @@ include("./sig/include/tools.php");
 <div class="container content-fluid">
   <div class="row">
     <div class="col-md-12 columns text-center firstdiv">
+    <!-- <h1>Work4Love.net</h1> -->
     <h1><img src="/sig/images/Work4Love.net-logo.png" alt="Work4Love.net" width="40%"/></h1>
-    <p class="bg-info text-left"><big>O site Work4Love.net é um portal composto por um diretório de sites por tema, banco de dados de notícias e links úteis, blog de viagens e serviços diversos.</big></p>
+    <p class="bg-info"><big>Work4Love.net - Portal de serviços: Banco de dados de Notícias, Sites sobre Finanças, Tecnologia, Viagens, Cursos e Entretenimento, Galeria de fotos de viagem e Blog Nomad Fire Life.</big></p>
       <div id="clock">day, dd/mm/yyyy hh:mm:ss</div>
       <script>
       setInterval(showTime, 120);
@@ -26,51 +27,17 @@ include("./sig/include/tools.php");
 
     <div class="col-md-12 columns text-center" style="padding-left:10%;padding-right:10%;padding-top:5%">
 
-    <p><a href="https://www.mensagembr.com" class="btn btn-lg btn-success" target="_blank">Envie sua mensagem positiva!</a><br/></p>
     <?php include("./sig/util/mensagembrOpenGraph.php"); ?>
+    <p><a href="https://www.mensagembr.com" class="btn btn-lg btn-success" target="_blank">Envie sua mensagem positiva!</a><br/></p>
     <br/>
 
-    <div class="col-md-12 columns text-center" style="padding-left:10%;padding-right:10%;padding-top:5%">
-      <p><a href="/sig/emma/" class="btn btn-lg btn-success">Directory</a></p>
-      <br/>
-      <div class="col-sm-3">
-        <span class="glyphicon glyphicon-star"></span>
-        <h4><a href="/sig/emma/">Sites</a></h4>
-      </div>
-      <div class="col-sm-3">
-        <span class="glyphicon glyphicon-usd"></span>
-        <h4><a href="/sig/rommie/">Finance</a></h4>
-      </div>
-      <div class="col-sm-3">
-        <span class="glyphicon glyphicon-education"></span>
-        <h4><a href="/sig/serina/">Learning</a></h4>
-      </div>
-      <div class="col-sm-3">
-        <span class="glyphicon glyphicon-picture"></span>
-        <h4><a href="/sig/finn/">Travel</a></h4>
-      </div>
-    </div>
+    <p>
+    <a href="/sig/emma/" class="btn btn-lg btn-success">Sites</a>
+    <br/>
+    Sites úteis: Finanças, Tecnologia, Viagens, Cursos e Entretenimento.
+    </p>
+    <br/>
 
-    <div class="col-md-12 columns text-center" style="padding-left:10%;padding-right:10%;padding-top:5%">
-      <div class="col-sm-3">
-        <span class="glyphicon glyphicon-film"></span>
-        <h4><a href="https://work4love.net/videos/" target="_blank">Videos</a></h4>
-      </div>
-      <div class="col-sm-3">
-        <span class="glyphicon glyphicon-phone"></span>
-        <h4><a href="/sig/elle/">Technology</a></h4>
-      </div>
-      <div class="col-sm-3">
-        <span class="glyphicon glyphicon-fire"></span>
-        <h4><a href="/sig/charlotte/">Formula 1</a></h4>
-      </div>
-      <div class="col-sm-3">
-        <span class="glyphicon glyphicon-music"></span>
-        <h4><a href="https://work4love.net/melody/" target="_blank">Music</a></h4>
-      </div>
-    </div>
-
-    <div class="col-md-12 columns text-center" style="padding-left:10%;padding-right:10%;padding-top:5%">
     <p><a href="/sig/news" class="btn btn-lg btn-success">Últimas Notícias</a><br/>
     <div align="left">
       <?php getExternalContent("https://work4love.net/sig/serina/news4.php")?>
@@ -87,11 +54,11 @@ include("./sig/include/tools.php");
     <p class="bg-info" align="center"><small>This spreadsheet is a courtesy of <strong><a href="https://rommiebr.com/" target="_blank">Rommie blog</a></strong>.</small></br></p>
     <br/>
 
-    <p><a href="https://work4love.net/nomad/" class="btn btn-lg btn-success" target="_blank">Blog de Viagens</a><br/></p>
+    <p><a href="/nomad/" class="btn btn-lg btn-success" target="_blank">Blog de Viagens</a><br/></p>
     <div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
       <?php include("./sig/include/carousel.php"); ?>
     </div>
-
+    
     <h2>Serviços</h2>
     <form method="post" action="https://sp35000.pythonanywhere.com/" target="_blank">
     <label for="text">Texto</label>
@@ -100,10 +67,10 @@ include("./sig/include/tools.php");
     </form>
     <br/>
 
-    <p><a href="https://work4love.net/calculator/" class="btn btn-lg btn-success" target="_blank">Calculadora</a><br/>
+    <p><a href="/calculator" class="btn btn-lg btn-success" target="_blank">Calculadora</a><br/>
         Calculadora básica (versão beta).
 
-    <p><a href="https://work4love.net/archive/" class="btn btn-lg btn-success">Arquivo</a><br/>
+    <p><a href="/archive" class="btn btn-lg btn-success">Arquivo</a><br/>
     Sites antigos republicados a pedido.
     </p>
 
